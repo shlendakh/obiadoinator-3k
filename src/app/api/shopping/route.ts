e256639f7b147d9135f1f48b5e8426330a1e5bb8
Paweł Szlendak <kontakt@pawelszlendak.pl>
2024-10-17 03:28:35 +0200

    shopping list functionality first attempts

---
+++ src/app/api/shopping/route.ts
@@ -0,0 +1,59 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
+
+export async function GET() {
+  const session = await getServerSession(authOptions);
+
+  // Return "unauthorized" if didn't catch session at all
+  if (!session) {
+    return NextResponse.json({ message: "Uga buga" }, { status: 401 });
+  }
+
+  try {
+    // Return message if didn't catch user email from session
+    const userEmail = session.user?.email;
+    if (!userEmail) {
+      return NextResponse.json(
+        { message: "User email is missing" },
+        { status: 400 }
+      );
+    }
+
+    // Check if user is member of any family
+    const familyMember = await prisma.familyMember.findFirst({
+      where: {
+        user: {
+          email: userEmail,
+        },
+      },
+      include: {
+        family: true,
+      },
+    });
+
+    // If user doesn't belong to any family
+    if (!familyMember) {
+      return NextResponse.json(
+        { message: "User does not belong to any family" },
+        { status: 200 }
+      );
+    }
+
+    const shoppingList = await prisma.shoppingList.findFirst({
+      where: {
+        familyId: familyMember.familyId,
+      },
+    });
+
+    // If everything is allright return info about family
+    return NextResponse.json({ shoppingList }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
--- src/app/shopping/page.tsx
+++ src/app/shopping/page.tsx
@@ -1,24 +1,65 @@
-// /app/shopping/page.tsx (strona serwerowa)
-import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/options";
-import Link from "next/link";
+"use client";
 
-export default async function Shopping() {
-  const session = await getServerSession(authOptions);
+import { useEffect, useState, useCallback } from "react";
+import { getSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
-  if (!session) {
-    // Zwróć stronę logowania, jeśli nie ma sesji
-    return (
-      <div>
-        Access Denied. Please <Link href="/api/auth/signin">log in</Link>.
-      </div>
-    );
+interface ShoppingListInfo {
+  shoppingList?: ShoppingList;
+  message?: string;
+}
+
+export default function Shopping() {
+  // Router
+  const router = useRouter();
+
+  // State
+  const [shoppingListInfo, setShoppingListInfo] =
+    useState<ShoppingListInfo | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const fetchShoppingListInfo = useCallback(async () => {
+    try {
+      const session = await getSession();
+      if (!session) {
+        // If user is not authenticated, redirect to login
+        router.push("/api/auth/signin");
+        return;
+      }
+      const response = await fetch("/api/shopping");
+      if (!response.ok) {
+        throw new Error("Failed to fetch family info");
+      }
+      const data = await response.json();
+      if (data.message) {
+        console.log(data.message);
+      }
+      setShoppingListInfo(data);
+    } catch (error) {
+      console.error("Failed to fetch family info", error);
+    } finally {
+      setLoading(false);
+    }
+  }, [router]);
+
+  useEffect(() => {
+    fetchShoppingListInfo();
+  }, [fetchShoppingListInfo]);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (!shoppingListInfo) {
+    return <p>Error loading shopping list information.</p>;
   }
 
   return (
     <div>
-      <h1>Welcome to Shopping, {session.user?.name}</h1>
-      {/* Reszta treści strony */}
+      <h1>Welcome to Shopping</h1>
+      <h2>name: {shoppingListInfo.shoppingList?.name}</h2>
+      <h2>id: {shoppingListInfo.shoppingList?.id}</h2>
+      <h2>familyId: {shoppingListInfo.shoppingList?.familyId}</h2>
     </div>
   );
 }
