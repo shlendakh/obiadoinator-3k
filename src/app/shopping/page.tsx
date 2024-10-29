55743ea87c443fb3eb759f2db1c6324c39a7f685
Paweł Szlendak <kontakt@pawelszlendak.pl>
2024-10-29 23:00:06 +0100

    major changes for SSR component

--- src/app/shopping/page.tsx
+++ src/app/shopping/page.tsx
@@ -1,11 +1,11 @@
-"use client";
-
-import { useEffect, useState, useCallback } from "react";
-import { getSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
-
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/options";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 import { ShoppingItem } from "./components/ShoppingItem/ShoppingItem";
 import { ItemCategory } from "@prisma/client";
+
+const baseUrl = process.env.NEXTAUTH_URL || "http://localhost:3000";
 
 interface ShoppingListInfo {
   shoppingList?: ShoppingList;
@@ -17,148 +17,123 @@ interface ProductsListInfo {
   message?: string;
 }
 
-export default function Shopping() {
-  // Router
-  const router = useRouter();
+/**
+ * Get shopping list info (for list id purpose and checking connection)
+ *
+ * @param {string} cookies Next-auth session token
+ * @returns {ShoppingListInfo} JSON with info or message if error
+ */
+async function getShoppingListData(cookie: string) {
+  const res = await fetch(`${baseUrl}/api/shopping/`, {
+    headers: { Cookie: cookie },
+  });
+  return res.json();
+}
 
-  // States
-  const [loading, setLoading] = useState<boolean>(true);
-  const [shoppingListInfo, setShoppingListInfo] =
-    useState<ShoppingListInfo | null>(null);
-  const [productsListInfo, setProductsListInfo] =
-    useState<ProductsListInfo | null>(null);
-  const [categoryList, setCategoryList] = useState<ItemCategory[]>([]);
+/**
+ * Return array of categories with names and icons
+ * @returns {ItemCategory[]} Array of categories
+ */
+async function getCategories() {
+  const res = await fetch(`${baseUrl}/api/info/category`);
+  const data = await res.json();
 
-  const fetchShoppingListInfo = useCallback(async () => {
-    try {
-      const session = await getSession();
-      if (!session) {
-        // If user is not authenticated, redirect to login
-        router.push("/api/auth/signin");
-        return;
-      }
-      const response = await fetch("/api/shopping");
-      if (!response.ok) {
-        throw new Error("Failed to fetch family info");
-      }
-      const data = await response.json();
-      if (data.message) {
-        console.log(data.message);
-      }
+  return data.categoryList;
+}
 
-      setShoppingListInfo(data);
-    } catch (error) {
-      console.error("Failed to fetch family info", error);
+/**
+ * Get all products from specific shopping list
+ *
+ * @param {string} cookie Next-auth session token
+ * @param {string} shoppingListId Shopping list id
+ * @returns {ProductsListInfo} JSON with info or message if error
+ */
+async function getProductListData(cookie: string, shoppingListId: string) {
+  const res = await fetch(
+    `${baseUrl}/api/shopping/products-list?shoppingListId=${shoppingListId}`,
+    {
+      headers: { Cookie: cookie },
+      cache: "no-cache",
     }
-  }, [router]);
-
-  const fetchProductsListInfo = useCallback(
-    async (shoppingListId: string) => {
-      try {
-        const session = await getSession();
-        if (!session) {
-          // If user is not authenticated, redirect to login
-          router.push("/api/auth/signin");
-          return;
-        }
-
-        const response = await fetch(
-          `/api/shopping/products-list?shoppingListId=${shoppingListId}`,
-          {
-            method: "GET",
-            headers: {
-              "Content-Type": "application/json",
-            },
-          }
-        );
-
-        if (!response.ok) {
-          throw new Error("Failed to fetch shopping list info");
-        }
-        const data = await response.json();
-        if (data.message) {
-          console.log(data.message);
-        }
-
-        setProductsListInfo(data);
-      } catch (error) {
-        console.error("Failed to fetch products list info", error);
-      } finally {
-        setLoading(false);
-      }
-    },
-    [router]
   );
+  return res.json();
+}
 
-  const fetchCategories = useCallback(async () => {
-    try {
-      const response = await fetch("/api/info/category");
-      if (!response.ok) {
-        throw new Error("Failed to fetch category info");
-      }
-      const data = await response.json();
-      setCategoryList(
-        Array.isArray(data.categoryList) ? data.categoryList : []
-      );
-    } catch (error) {
-      console.error("Failed to fetch category info", error);
-    }
-  }, []);
+export default async function Page() {
+  const session = await getServerSession(authOptions);
 
-  useEffect(() => {
-    fetchShoppingListInfo();
-    fetchCategories();
-  }, [fetchShoppingListInfo, fetchCategories]);
-
-  useEffect(() => {
-    if (shoppingListInfo?.shoppingList?.id) {
-      fetchProductsListInfo(shoppingListInfo.shoppingList.id);
-    }
-  }, [shoppingListInfo, fetchProductsListInfo]);
-
-  if (loading) {
-    return <p>Loading...</p>;
+  // If not logged redirect for sign in
+  if (!session) {
+    redirect("/api/auth/signin");
   }
 
-  if (!shoppingListInfo) {
-    return <p>Error loading shopping list information.</p>;
-  }
+  // Fetch cookie as string
+  const cookie = cookies()
+    .getAll()
+    .map((c) => `${c.name}=${c.value}`)
+    .join("; ");
 
-  return (
-    <>
-      {/* <div className="p-4">
-        <h1 className="mb-4 text-2xl font-bold">Categories</h1>
-        {categoryList.length === 0 ? (
-          <p>No categories found.</p>
-        ) : (
-          <div className="flex flex-wrap gap-4">
-            {categoryList.map((category) => (
-              <div
-                key={category.id}
-                className="flex items-center gap-2 p-2 border rounded-md"
-              >
-                <span>{category.categoryName}</span>
-              </div>
-            ))}
-          </div>
-        )}
-      </div> */}
+  // Fetch initial data async
+  const [shoppingListInfo, categories]: [ShoppingListInfo, ItemCategory[]] =
+    await Promise.all([getShoppingListData(cookie), getCategories()]);
+
+  // Return shopping list message if there's any
+  if (shoppingListInfo.message) {
+    return (
       <div className="p-4">
         <h1 className="mb-4 text-2xl font-bold">Shopping List</h1>
-        {productsListInfo?.productsList?.length === 0 ? (
-          <p>No items found in the shopping list.</p>
-        ) : (
-          <div className="flex flex-col items-center justify-center gap-2">
-            {productsListInfo?.productsList?.map((item) => (
-              <ShoppingItem
-                key={item.id}
-                params={item}
-                categoryList={categoryList}
-              />
-            ))}
-          </div>
-        )}
+        <p>{shoppingListInfo.message}</p>
       </div>
-    </>
+    );
+  }
+
+  // Return error if there's not any id in shopping list info
+  if (!shoppingListInfo.shoppingList?.id) {
+    return (
+      <div className="p-4">
+        <h1 className="mb-4 text-2xl font-bold">Shopping List</h1>
+        <p>Some problem fetching shopping list ID</p>
+      </div>
+    );
+  }
+
+  // Fetch list of products for user list
+  const productsList: ProductsListInfo = await getProductListData(
+    cookie,
+    shoppingListInfo.shoppingList.id
+  );
+
+  let products: ShoppingItem[] | undefined = undefined;
+
+  // Handle error from fetching products list
+  if (productsList.message) {
+    return (
+      <div className="p-4">
+        <h1 className="mb-4 text-2xl font-bold">Shopping List</h1>
+        <p>{productsList.message}</p>
+      </div>
+    );
+  } else {
+    products = productsList.productsList;
+  }
+
+  // Main render
+  return (
+    <div className="p-4">
+      <h1 className="mb-4 text-2xl font-bold">Shopping List</h1>
+      {products?.length ? (
+        <div className="flex flex-col items-center justify-center gap-2">
+          {products.map((item) => (
+            <ShoppingItem
+              key={item.id}
+              params={item}
+              categoryList={categories}
+            />
+          ))}
+        </div>
+      ) : (
+        <p>No items found in the shopping list.</p>
+      )}
+    </div>
   );
 }
